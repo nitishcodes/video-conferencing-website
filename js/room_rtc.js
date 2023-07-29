76b0f596eadfd66a6a6d08f1ed3424d33098df33
nitishcodes <nitish532002@gmail.com>
2023-07-29 13:56:57 +0530

    Commit after adding SDK

---
+++ js/room_rtc.js
@@ -0,0 +1,42 @@
+const APP_ID = "9b35097313a642439feb7e70efc01ecf";
+
+let uid = sessionStorage.getItem("uid");
+
+if (!uid) {
+  uid = String(Math.floor(Math.random() * 10000));
+  sessionStorage.setItem("uid", uid);
+}
+
+let token = null;
+let client;
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+let roomId = urlParams.get("room");
+
+if (!roomId) {
+  roomId = "main";
+}
+
+let localTracks = [];
+let remoteUsers = {};
+
+let joinRoomInit = async () => {
+  client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
+  await client.join(APP_ID, roomId, token, uid);
+  joinStream();
+};
+
+let joinStream = async () => {
+  localTracks = await AgoraRTC.createMicrophoneAndCameraTracks();
+  let player = `<div class="video__container" id="user-container-${uid}">
+                    <div class="video__player" id="user-${uid}"></div>
+                </div>`;
+  document
+    .getElementById("streams__container")
+    .insertAdjacentHTML("beforeend", player);
+};
+console.log(`user-${uid}`);
+localTracks[1].play(`user-${uid}`);
+
+joinRoomInit();
