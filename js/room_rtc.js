d9f177597ee175edc9385bc42ba55992ceac2eb2
nitishcodes <nitish532002@gmail.com>
2023-07-31 15:28:42 +0530

    Creating Rooms

--- js/room_rtc.js
+++ js/room_rtc.js
@@ -17,6 +17,10 @@ let roomId = urlParams.get("room");
 if (!roomId) {
   roomId = "main";
 }
+let displayName = sessionStorage.getItem('display_name')
+if (!displayName) {
+  window.location = 'lobby.html'
+}
 
 let localTracks = [];
 let remoteUsers = {};
